c68d027c038886e1bc116b407998eda4c3e560b9
Giuseppe <contact.giuseppe00@gmail.com>
2025-07-11 02:53:47 +0100

    Initial commit with favorites_quiz.py

---
+++ favorites_quiz.py
@@ -0,0 +1,16 @@
+# favorites_quiz.py
+# A simple interactive quiz made by Giuseppe
+
+print("🎉 Welcome to the Favorite Things Quiz! 🎉")
+
+name = input("What's your name? ")
+color = input("What's your favorite color? ")
+food = input("What's your favorite food? ")
+hobby = input("What's a hobby you enjoy? ")
+
+print("\nThanks for playing, " + name + "!")
+print("Here's what we learned about you:")
+print("- Favorite color:", color)
+print("- Favorite food:", food)
+print("- Hobby:", hobby)
+print("You're awesome, " + name + "! Keep enjoying your favorite things. 😊")
